05910b59810b85d88618ab10f345e30b0a52908b
tinyboyX <phamgiang2811@gmail.com>
2019-11-09 15:58:21 +0700

    Update pratical 1

---
+++ USTHWeather/app/src/main/java/vn/edu/usthweather/WeatherActivity.java
@@ -0,0 +1,43 @@
+package vn.edu.usthweather;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class WeatherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Question","Are you ok ?");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
+
