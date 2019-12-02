46613411de5e6dd19aacbb19fe3861097d5619b1
phamgiang <phamgiang@gmail.com>
2019-12-02 22:49:38 +0700

    finnish practical work 12

--- USTHWeather/app/src/main/java/vn/edu/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usthweather/WeatherActivity.java
@@ -1,19 +1,30 @@
 package vn.edu.usthweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "WeatherActivity";
     private Adapter adapter;
     private ViewPager viewPager;
+    public static final String EXTRA_MESSAGE = "vn.edu.usthweather.MESSAGE";
+
     public WeatherActivity() {
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +34,35 @@ public class WeatherActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.weatherForecastViewPager);
         adapter = new Adapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
-        MediaPlayer mediaPlayer= MediaPlayer.create(this,R.raw.song);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.song);
         mediaPlayer.start();
 //        // Create a new Fragment to be placed in the activity
 //        ForecastFragment firstFragment = new ForecastFragment();
 //        // Add the fragment to the 'container' FrameLayout
 //        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.weather_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.setting_action: {
+                // do your sign-out stuff
+                startActivities(new Intent[]{new Intent(getApplicationContext(), PrefActivity.class)});
+                break;
+            }
+            case R.id.action_refresh: {
+                Toast.makeText(getApplicationContext(), R.string.refresh_message, Toast.LENGTH_LONG).show();
+                break;
+            }
+            // case blocks for other MenuItems (if any)
+        }
+        return true;
     }
 
     @Override
